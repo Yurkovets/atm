a6ea8f0fdbb671a1df16a35204b00e939e99f763
Hanna Malmyha <anna.yurkovets2@gmail.com>
2025-03-07 15:38:42 +0300

    added correct amount input check

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -14,15 +15,31 @@ func main() {
 		fmt.Println("2 - exit")
 
 		var option int
+		var withdrawalAmount int
 		fmt.Scan(&option)
+
 		switch option {
 		case 1:
-			fmt.Println("Available baknotes: ", banknotes)
+			fmt.Println("Enter the withdrawal amount: ")
+			fmt.Scan(&withdrawalAmount)
+			if math.Mod(float64(withdrawalAmount), 5) != 0 {
+				fmt.Println("Incorrect amount. Available denominations:")
+				printAvailableDenominations(banknotes)
+			}
+
 		case 2:
 			return
 		default:
 			fmt.Println("No such option exists")
 			fmt.Println(option)
+		}
+	}
+}
+
+func printAvailableDenominations(banknotes map[int]int) {
+	for denomination, amount := range banknotes {
+		if amount > 0 {
+			fmt.Println(denomination)
 		}
 	}
 }
